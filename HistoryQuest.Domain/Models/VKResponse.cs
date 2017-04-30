7827b397e0b6fd3d4fef201e127a818f90abaf88
vladloza <vladok001@gmail.com>
2017-04-30 14:08:58 +0300

    work with VK

---
+++ HistoryQuest.Domain/Models/VKResponse.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HistoryQuest.Domain.Models
+{
+    public class VKResponse
+    {
+        public VKResponse()
+        {
+        }
+
+        public VKResponse(string url)
+        {
+            System.Net.ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+            System.Net.WebRequest reqGET = System.Net.WebRequest.Create(url);
+            System.Net.WebResponse resp = reqGET.GetResponse();
+            System.IO.Stream stream = resp.GetResponseStream();
+            System.IO.StreamReader sr = new System.IO.StreamReader(stream);
+            string html = sr.ReadToEnd();
+            string parsed = JObject.Parse(html).ToString();
+            var result = JsonConvert.DeserializeObject<VKResponse>(parsed);
+
+            access_token = result.access_token;
+            expires_in = result.expires_in;
+            user_id = result.user_id;
+            email = result.email;
+        }
+
+        public string access_token;
+        public int expires_in;
+        public int user_id;
+        public string email;
+    }
+}
