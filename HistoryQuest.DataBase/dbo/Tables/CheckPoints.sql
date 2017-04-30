b17335c1c32fd316fefd1d822754b9588aec3ae2
Sergey M <sergey777neo@gmail.com>
2017-04-30 14:07:23 +0300

    fix db scheme (TasksToTries and CheckPoints)

--- HistoryQuest.DataBase/dbo/Tables/CheckPoints.sql
+++ HistoryQuest.DataBase/dbo/Tables/CheckPoints.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[CheckPoints] (
     [id]             BIGINT           IDENTITY (1, 1) NOT NULL,
     [gid]            UNIQUEIDENTIFIER NOT NULL,
+    [QuestGID]       UNIQUEIDENTIFIER NOT NULL,
     [Name]           NVARCHAR (250)   NOT NULL,
     [GeoCoordinates] NVARCHAR (250)   NOT NULL,
     [InfoFilePath]   NVARCHAR (250)   NOT NULL,
@@ -11,6 +12,9 @@
     [AuthorGID]      UNIQUEIDENTIFIER NULL,
     CONSTRAINT [PK_CheckPoints] PRIMARY KEY CLUSTERED ([gid] ASC),
     CONSTRAINT [FK_CheckPoints_CheckPoints] FOREIGN KEY ([ParentGID]) REFERENCES [dbo].[CheckPoints] ([gid]),
+    CONSTRAINT [FK_CheckPoints_Quests] FOREIGN KEY ([QuestGID]) REFERENCES [dbo].[Quests] ([gid]),
     CONSTRAINT [FK_CheckPoints_Users] FOREIGN KEY ([AuthorGID]) REFERENCES [dbo].[Users] ([gid])
 );
+
+
 
