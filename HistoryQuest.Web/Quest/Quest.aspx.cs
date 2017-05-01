ef61802f89ac654628f7036dc4dfe118aad3a43f
vladloza <vladok001@gmail.com>
2017-05-01 16:05:30 +0300

    fix

--- HistoryQuest.Web/Quest/Quest.aspx.cs
+++ HistoryQuest.Web/Quest/Quest.aspx.cs
@@ -7,11 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace HistoryQuest
 {
-    public partial class Quest : System.Web.UI.Page
+    public partial class Quest : BasePage
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-
-        }
     }
 }
