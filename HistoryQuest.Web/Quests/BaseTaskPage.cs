cf2468cfe60f4e7780fd1dddbb9ffe6d0b075c96
Sergey M <sergey777neo@gmail.com>
2017-05-14 21:27:08 +0300

    fix progress bar

--- HistoryQuest.Web/Quests/BaseTaskPage.cs
+++ HistoryQuest.Web/Quests/BaseTaskPage.cs
@@ -30,7 +30,7 @@ namespace HistoryQuest.Quests
                     tasksMaster.TaskDivControl.Attributes["TaskGID"] = task.gid.ToString();
                     tasksMaster.TaskDivControl.Attributes["TaskTypeGID"] = task.TaskTypeGID.ToString();
                     tasksMaster.TaskNumberControl.InnerText = string.Format("{0} из {1}", currentTaskId + 1, tasksList.Count);
-                    tasksMaster.ProgressBarControl.Attributes["style"] = string.Format("width: {0}%", 100 * (currentTaskId) / (float)tasksList.Count);
+                    tasksMaster.ProgressBarControl.Attributes["style"] = string.Format("width: {0}%", 100 * (currentTaskId) / (float)tasksList.Count).Replace(",", ".");
                 }
             }
             else
